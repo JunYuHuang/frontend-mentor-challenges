73cedcd122c33b099a04398367e7374c5b0f07a0
Jun Huang <junyu_huang@hotmail.com>
2020-02-08 17:06:38 -0800

    work on mobile view - complete all sections except join or email subscription

--- bookmark-landing-page-master/app.js
+++ bookmark-landing-page-master/app.js
@@ -2,18 +2,26 @@
 const logoImageText = document.querySelector(".logo-image__text");
 const logoImageIconSymbol = document.querySelector(".logo-image__icon-symbol");
 const logoImageIconCircle = document.querySelector(".logo-image__icon-circle");
+
 const navMenu = document.querySelector(".nav-menu");
 const navMenuButton = document.querySelector(".nav-menu-button");
 const navMenuButtonIcon = document.querySelector(".nav-menu-button-icon");
+
 const body = document.querySelector("body");
+
+const tabControlPanel = document.querySelector(".tab__controls");
+const tabControlButtons = document.querySelectorAll(".button--tab");
+const tabDisplayItems = document.querySelectorAll(".tab__display__item");
+
 const accordionButtons = document.querySelectorAll(".button--accordion");
+
+const form = document.querySelector("form");
 
 // EVENT LISTENERS //
 navMenuButton.addEventListener("click", () => {
-  // toggle nav menu items
   const navMenuActive = document.querySelector(".nav-menu--active");
   if (!navMenuActive) {
-    body.style.overflow = "hidden";
+    body.style.overflow = "hidden"; // prevents scrolling
   } else {
     body.style.overflow = "auto";
   }
@@ -28,10 +36,9 @@ accordionButtons.forEach(accordionButton => {
   });
 });
 
-// auto-close menu if any of its links is clicked in
+// auto-closes menu if any of its links is clicked on
 navMenu.addEventListener("click", e => {
   let isInMobile = matchMedia("only screen and (max-width: 851px)").matches;
-  console.log(isInMobile);
   if (isInMobile) {
     let isNavMenuItemLink = e.target.parentNode.classList.contains(
       "nav-menu__item"
@@ -47,11 +54,22 @@ navMenu.addEventListener("click", e => {
   }
 });
 
+tabControlPanel.addEventListener("click", e => {
+  toggleTabButton(e);
+  toggleTabDisplayItem(e);
+});
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  console.log("email submitted");
+});
+
 // FUNCTIONS //
 function toggleNavButton() {
   let currentNavMenuButtonIcon = document.querySelector(
     ".nav-menu-button-icon"
   );
+  // toggle menu icon between hamburger and x/close
   if (
     currentNavMenuButtonIcon.getAttribute("src") ===
     "./images/icon-hamburger.svg"
@@ -61,7 +79,7 @@ function toggleNavButton() {
     currentNavMenuButtonIcon.setAttribute("src", "./images/icon-hamburger.svg");
   }
   navMenuButtonIcon.classList.toggle("nav-menu-button-icon--close");
-  navMenu.classList.toggle("nav-menu--active");
+  navMenu.classList.toggle("nav-menu--active"); // open/close nav menu
 }
 
 function toggleLogoColours() {
@@ -72,9 +90,48 @@ function toggleLogoColours() {
 
 function toggleAccordionItem(e) {
   let clickedElement = e.target;
+  // open/close accordion
   clickedElement.parentNode.classList.toggle("accordion__item--active");
-  clickedElement.firstChild.nextSibling.firstChild.nextSibling.classList.toggle(
+  // rotate arrow icon up or down
+  clickedElement.lastChild.previousSibling.firstChild.nextSibling.classList.toggle(
     "accordion__item__icon__stroke--active"
   );
-  console.log(clickedElement.firstChild.nextSibling.firstChild.nextSibling);
+}
+
+function toggleTabButton(e) {
+  // allow only one tab button to be "active" at any time
+  let clickedTabButton = e.target;
+  let isTabButton = clickedTabButton.classList.contains("button--tab");
+
+  if (isTabButton) {
+    if (!clickedTabButton.classList.contains("button--tab--active")) {
+      clickedTabButton.classList.toggle("button--tab--active");
+    }
+
+    tabControlButtons.forEach(tabControlButton => {
+      if (tabControlButton !== clickedTabButton) {
+        if (tabControlButton.classList.contains("button--tab--active")) {
+          tabControlButton.classList.toggle("button--tab--active");
+        }
+      }
+    });
+  }
+}
+
+function toggleTabDisplayItem(e) {
+  // get the target display item
+  // allow only one tab display item to be "active" at any time
+  let targetTabDisplayItemID = e.target.dataset.tabDisplayItemTarget;
+
+  tabDisplayItems.forEach(tabDisplayItem => {
+    if (tabDisplayItem.classList.contains(targetTabDisplayItemID)) {
+      if (!tabDisplayItem.classList.contains("tab__display__item--active")) {
+        tabDisplayItem.classList.toggle("tab__display__item--active");
+      }
+    } else {
+      if (tabDisplayItem.classList.contains("tab__display__item--active")) {
+        tabDisplayItem.classList.toggle("tab__display__item--active");
+      }
+    }
+  });
 }
