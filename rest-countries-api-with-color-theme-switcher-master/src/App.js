772e6f11129308858a99a94a24aa33581e0ae0a9
Jun Huang <junyu_huang@hotmail.com>
2020-03-06 14:10:59 -0800

    add basic api call to home page

--- rest-countries-api-with-color-theme-switcher-master/src/App.js
+++ rest-countries-api-with-color-theme-switcher-master/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import DetailPage from "./pages/DetailPage";
@@ -6,13 +6,42 @@ import Header from "./common/Header";
 import history from "./history";
 
 function App() {
+  // state
+  const [countriesList, setCountriesList] = useState([]);
+  const [country, setCountry] = useState([]);
+  const [countryNameFilterTerm, setCountryNameFilterTerm] = useState("");
+  const [countryRegionFilterTerm, setCountryRegionFilterTerm] = useState("");
+
+  // global consts and vars
+  const BASE_API_URL = "https://restcountries.eu/rest/v2";
+  const HOME_PAGE_API_URL =
+    BASE_API_URL + "/all?fields=alpha3Code;flag;name;population;region;capital";
+  const DETAIL_PAGE_API_URL =
+    BASE_API_URL +
+    "/all?fields=alpha3Code;flag;name;nativeName;population;region;subregion;capital;topLevelDomain;currencies;languages;borders;";
+
+  // api call for country list for home page
+  useEffect(() => {
+    fetch(HOME_PAGE_API_URL)
+      .then(res => res.json())
+      .then(res => {
+        setCountriesList(res);
+        console.log(countriesList);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <div className="main-wrapper">
       <Router history={history}>
         <Header />
         <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/countries/:id" component={DetailPage} />
+          <Route exact path="/">
+            <HomePage countries={countriesList} />
+          </Route>
+          <Route exact path="/countries/:id">
+            <DetailPage />
+          </Route>
         </Switch>
       </Router>
     </div>
--- rest-countries-api-with-color-theme-switcher-master/src/pages/HomePage.js
+++ rest-countries-api-with-color-theme-switcher-master/src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const HomePage = () => {
+const HomePage = ({ countries }) => {
   function toggleDropdownMenu() {
     const chevronIcon = document.querySelector(".icon-container--dropdown");
     const dropdownList = document.querySelector(".input-group--dropdown__list");
@@ -39,31 +39,14 @@ const HomePage = () => {
     );
   });
 
-  const countries = [
-    {
-      name: "Germany",
-      population: "81,770,900",
-      region: "Europe",
-      capital: "Berlin",
-      flag: "https://upload.wikimedia.org/wikipedia/en/b/ba/Flag_of_Germany.svg"
-    },
-    {
-      name: "United States of America",
-      population: "323,947,000",
-      region: "Americas",
-      capital: "Washington, D.C.",
-      flag:
-        "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1920px-Flag_of_the_United_States.svg.png"
-    }
-  ];
   const countriesList = countries.map(country => {
     return (
-      <div className="card">
+      <div className="card" data-country-id={country.alpha3Code}>
         <div className="card__img-container">
           <img src={country.flag} alt={`The flag of ${country.name}`} />
         </div>
-        <section className="card__text">
-          <h2 className="card__text__title">{country.name}</h2>
+        <section className="card__text ellipsis">
+          <h2 className="card__text__title ellipsis">{country.name}</h2>
           <p className="card__text__fact">
             <strong>Population:</strong> {country.population}
           </p>
